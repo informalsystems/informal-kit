cb539f03cdbbdb2ca68f090a95c185fe1b6dbffa
Aaron Wright <aaronmw@gmail.com>
2025-08-09 20:28:44 -0400

    Augment ThemeProvider to accept selectors

--- components/ThemeProvider.tsx
+++ components/ThemeProvider.tsx
@@ -5,16 +5,25 @@ import { useEffect } from 'react'
 import { getClassNameFromPathname } from '../lib/useRouteClassName'
 
 interface ThemeProviderProps {
-  routeClassNameMap: Record<string, string>
+  routeClassNameMap: Record<string, Record<string, string>>
 }
 
 export function ThemeProvider({ routeClassNameMap }: ThemeProviderProps) {
   const pathname = usePathname()
-  const routeClassName = getClassNameFromPathname(pathname, routeClassNameMap)
 
   useEffect(() => {
-    document.documentElement.className = routeClassName
-  }, [pathname, routeClassName])
+    Object.entries(routeClassNameMap).forEach(([selector, routeClassMap]) => {
+      const nextClassName = getClassNameFromPathname(pathname, routeClassMap)
+      const element = document.querySelector(selector) as HTMLElement | null
+      if (!element) return
+
+      const allRouteClasses = Object.values(routeClassMap)
+      allRouteClasses.forEach(classToken => {
+        if (classToken) element.classList.remove(classToken)
+      })
+      if (nextClassName) element.classList.add(nextClassName)
+    })
+  }, [pathname, routeClassNameMap])
 
   return null
 }
